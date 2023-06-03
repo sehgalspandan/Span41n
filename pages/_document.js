8a25da5d17ee3e638a1f8ab6f0bd7d6356b7f02f
Spandan <spandansehgal@gmail.com>
2023-06-03 16:04:18 +0530

    update _doc.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,13 +7,9 @@ export default function Document() {
       <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com&display=optional"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-<<<<<<< HEAD
         <link href="https://fonts.googleapis.com/css2?family=Sarina&display=swap" rel="stylesheet"></link>
         <script src="https://kit.fontawesome.com/c151b27f34.js" crossorigin="anonymous" async></script>
 
-=======
-        <link href="https://fonts.googleapis.com/css2?family=Sarina&display=swap" rel="stylesheet" />
->>>>>>> 119359fb0816a0006a8cbb4986f400131411cb67
       </Head>
       <body>
    <Main />
