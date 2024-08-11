802ae8803c5173701da95f571a477cebfa92b165
Spandan <spandansehgal@gmail.com>
2024-08-11 13:53:00 +0530

    Added posthog

--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Navbar />
+
       <br/>
       <main>{children}
       <Analytics/>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Script from "next/script";
 
+import posthog from "posthog-js"
+import { PostHogProvider } from 'posthog-js/react'
 
+if (typeof window !== 'undefined') { // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
+    },
+  })
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -23,7 +34,9 @@ page_path: window.location.pathname,
 `,
             }}
         />
+        <PostHogProvider client={posthog}>
         <Component {...pageProps} />
+        </PostHogProvider>
 
       </>
   )
